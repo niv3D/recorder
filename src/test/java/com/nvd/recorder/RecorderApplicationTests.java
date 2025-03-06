10eafb59bac1e0dee5f7a9fc906292c1c123ef4d
niv3D <nived807@gmail.com>
2025-03-07 01:08:19 +0530

    first commit

---
+++ src/test/java/com/nvd/recorder/RecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvd.recorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
